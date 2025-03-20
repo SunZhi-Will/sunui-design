e58dbb9bf18a20698b1a4a0d6dc59f75372a5b80
Sunzhi Xie <sun055676@gmail.com>
2025-03-20 17:26:43 +0800

    chore: update package versions and dependencies, including @sunui-design/core to 0.0.47, and add new modules in package-lock.json

--- packages/card/tsup.config.ts
+++ packages/card/tsup.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts"],
-    format: ["cjs", "esm"],
+    format: ["esm", "cjs"],
     dts: true,
     splitting: false,
     sourcemap: true,
     clean: true,
+    treeshake: true,
+    minify: true,
     external: ["react"],
 }); 
